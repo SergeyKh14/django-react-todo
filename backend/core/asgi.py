9cc9739736c5b1d2b0e03777a36a8b43446348ee
Sergey Kharatyan <45818392+SergeyKh14@users.noreply.github.com>
2026-03-14 21:27:48 +0400

    fix: initialize Django app registry before other imports in asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -9,14 +9,15 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+from django.core.asgi import get_asgi_application  # noqa: E402
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
 from django.core.asgi import get_asgi_application  # noqa: E402
 
 from core.middleware import JWTWebsocketAuthMiddleware  # noqa: E402
 from core.routing import websocket_urlpatterns  # noqa: E402
-
-# Django ASGI application for HTTP
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
